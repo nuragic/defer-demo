f232881cf767f66fa3c5df326f43a88b5aa01da6
clarencenpy <clarencenpy@gmail.com>
2018-07-17 02:12:31 -0700

    Most compact syntax for checking loadingState

--- client/src/Components.js
+++ client/src/Components.js
@@ -9,42 +9,29 @@ const borderStyle = {
 
 export const Weapon = ({ weapon, loadingState }) => (
   <div>
-    <p>Name: {loadingState.name._isLoaded ? weapon.name : '...'}</p>
-    <p>
-      Strength:{' '}
-      {loadingState.strength._isLoaded ? weapon.strength : '...' }
-    </p>
+    <p>Name: {loadingState.name ? weapon.name : '...'}</p>
+    <p>Strength: {loadingState.strength ? weapon.strength : '...'}</p>
   </div>
 );
 
 export const CharacterCard = ({ character, loadingState }) => (
   <div style={borderStyle}>
     <p>ID: {character.id}</p>
-    <p>
-      Name:{' '}
-      {loadingState.name._isLoaded ? character.name : '...'}
-    </p>
+    <p>Name: {loadingState.name ? character.name : '...'}</p>
 
     <div style={borderStyle}>
-      {loadingState.friends._isLoaded
+      {loadingState.friends
         ? character.friends.map((friend, i) => (
-          <p key={friend.id}>
-            {friend.id}:{' '}
-            {loadingState.friends[i].name._isLoaded
-              ? friend.name
-              : '...'}
-          </p>
-        ))
-        : 'loading friends...'
-      }
+            <p key={friend.id}>
+              {friend.id}: {loadingState.friends[i].name ? friend.name : '...'}
+            </p>
+          ))
+        : 'loading friends...'}
     </div>
 
     <div style={borderStyle}>
-      {loadingState.weapon._isLoaded ? (
-        <Weapon
-          weapon={character.weapon}
-          loadingState={loadingState.weapon}
-        />
+      {loadingState.weapon ? (
+        <Weapon weapon={character.weapon} loadingState={loadingState.weapon} />
       ) : (
         'loading weapon...'
       )}
