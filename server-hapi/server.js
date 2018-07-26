c4cbef67c20f046d0354a2a09a6a8c5ba662b02d
clarencenpy <clarencenpy@gmail.com>
2018-07-25 22:04:12 -0700

    Add examples for Koa and Hapi variants

---
+++ server-hapi/server.js
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require('apollo-server-hapi');
+const Hapi = require('hapi');
+const { typeDefs, resolvers } = require('./newsFeedSchema');
+
+async function StartServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  const app = new Hapi.server({
+    port: 4000
+  });
+
+  await server.applyMiddleware({
+    app,
+  });
+
+  await server.installSubscriptionHandlers(app.listener);
+
+  await app.start();
+}
+
+StartServer().catch(error => console.log(error));
---
+++ server-koa/server.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const { ApolloServer } = require('apollo-server-koa');
+const { typeDefs, resolvers } = require('./newsFeedSchema');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = new Koa();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
